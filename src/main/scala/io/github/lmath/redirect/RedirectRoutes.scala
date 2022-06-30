77f35d7915caae05969b85c4651268883c3342be
Leigh-Anne Mathieson <leigh-anne@snowplowanalytics.com>
2022-06-30 17:50:07 +0100

    Add decoder to recognise repr query param

--- src/main/scala/io/github/lmath/redirect/RedirectRoutes.scala
+++ src/main/scala/io/github/lmath/redirect/RedirectRoutes.scala
@@ -4,20 +4,27 @@ import cats.effect.Sync
 import cats.implicits.catsSyntaxApplicativeId
 import org.http4s.{Header, Headers, HttpRoutes, Response, Uri}
 import org.http4s.dsl.Http4sDsl
+import org.http4s.dsl.impl.OptionalQueryParamDecoderMatcher
 import org.typelevel.ci.CIString
 
 object RedirectRoutes {
 
+  object ReprQueryParamMatcher extends OptionalQueryParamDecoderMatcher[String]("repr")
+
   def redirectRoute[F[_]: Sync](): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F]{}
     import dsl._
-    def response(path: Uri.Path) = {
-      Response[F]().withStatus(PermanentRedirect).withHeaders(Headers(Header.Raw(CIString("Location"), s"http://localhost:8080/api/${path.renderString}")))
+    def response(path: Uri.Path, reprParam: Option[String]) = {
+      val location = s"http://localhost:8080/api/${path.renderString}"
+      val locationString = reprParam match {
+        case Some(repr) => s"$location?repr=$repr"
+        case None => location
+      }
+      Response[F]().withStatus(PermanentRedirect).withHeaders(Headers(Header.Raw(CIString("Location"), locationString)))
     }
 
     HttpRoutes.of[F] {
-      case _ -> "api" /: rest =>
-        response(rest).pure[F]
+      case _ -> "api" /: rest :? ReprQueryParamMatcher(repr) => response(rest, repr).pure[F]
     }
 
   }
